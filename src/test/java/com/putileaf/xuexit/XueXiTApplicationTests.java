1020cbf5e9beb600a117926ed0aab9b059dbb023
29030 <2903039102@qq.com>
2025-03-26 14:53:27 +0800

    3-26

--- src/test/java/com/putileaf/xuexit/XueXiTApplicationTests.java
+++ src/test/java/com/putileaf/xuexit/XueXiTApplicationTests.java
@@ -10,7 +10,7 @@ class XueXiTApplicationTests {
     @Autowired private TimeService timeService;
     @Test
     void contextLoads() {
-        timeService.time0801();
+        timeService.time0900();
 
     }
 
