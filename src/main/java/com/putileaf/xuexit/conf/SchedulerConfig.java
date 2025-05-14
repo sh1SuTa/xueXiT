c15ce5443710859d385b44daa8c3d73c8b3fa53d
29030 <2903039102@qq.com>
2025-05-14 09:04:46 +0800

    多线程

---
+++ src/main/java/com/putileaf/xuexit/conf/SchedulerConfig.java
@@ -0,0 +1,18 @@
+package com.putileaf.xuexit.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.Executors;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig implements SchedulingConfigurer {
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setScheduler(Executors.newScheduledThreadPool(5)); // 设置5个线程的线程池
+    }
+}
